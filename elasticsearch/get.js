7417525b2fed47041d37074fdb7ecb457176fb71
vvw <siluplsy@gmail.com>
2015-01-12 11:12:22 +0800

    put

--- elasticsearch/get.js
+++ elasticsearch/get.js
@@ -15,25 +15,48 @@ function s2j(s) {
     return JSON.parse(s);
 }
 
+// pretty format josn
+function pretty (json) {
+	return prettyjson.render(json, {noColor: true})	
+}
+
 function data(fname) {
     return fs.readFileSync(fname, 'utf-8');
 }
 
 var html = data("0,8816,881304,00.html");
-//console.log ( data("0,8816,881304,00.html") );
 
 function get(url, cb) {
-  var re = {err : false};
-  http.get(url, function(res) {
-    re.statusCode = res.statusCode;
-    res.on("data", function(chunk) {
-      re.data = s2j(chunk.toString());
-      cb (re);
-    });
-  }).on('error', function(e) {
-    re.err = e.message;
-    cb (re);
-  });
+	var re = {err : false};
+	request(
+  		{
+  			url: url,
+    		method: 'get'
+  		},
+  		function(error, request, body) {
+    		if (error) {re.err = error.message;}
+    		re.statusCode = request.statusCode;
+    		re.data = s2j(body.toString());
+    		cb (re);
+  		}
+	)
+}
+
+function put(url, json, cb) {
+	var re = {err : false};
+	request(
+  		{
+  			url: url,
+    		method: 'PUT',
+    		json: j
+  		},
+  		function(error, request, body) {
+    		if (error) {re.err = error.message;}
+    		re.statusCode = request.statusCode;
+    		re.data = body;
+    		cb (re);
+  		}
+	);
 }
 
 var j = {
@@ -42,30 +65,12 @@ var j = {
 	tags : ["time", "1923", "Congress"] 
 }
 
-request(
-  { url: 'http://127.0.0.1:9200/magazine/time/1',
-    method: 'PUT',
-    json: j
-  },
-  function(error, request, body) {
-    if (error) console.log(error);
-    //console.log (request.statusCode);
-    //console.log(body);
-  }
-)
+ put ('http://127.0.0.1:9200/magazine/time/1', j, function (re) {
+	console.log(pretty(re));
+	get ('http://127.0.0.1:9200/magazine/time/1', function (re) {
+		console.log(pretty(re));
+ 	})
+ })
 
-get ("http://127.0.0.1:9200/_cluster/health?pretty", function(re) {
-  //console.log (re);
-  console.log(prettyjson.render(re, {noColor: true}));
-});
-
-get ("http://127.0.0.1:9200/magazine/time/1", function(re) {
-  //console.log (re);
-});
-
-// (function(){
-//   "use strict";
-// })();
-
-console.log('hi,,')
+console.log ('hi,,')
 
